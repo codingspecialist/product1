f706ac7ddc2bc654adfba8cde0fd537475e8072d
getinthere <getinthere@naver.com>
2023-01-17 12:42:09 +0900

    mybatis 상품 프로그램 완료

---
+++ src/main/java/shop/mtcoding/orange2/Orange2Application.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.orange2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Orange2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Orange2Application.class, args);
+	}
+
+}
---
+++ src/test/java/shop/mtcoding/orange2/Orange2ApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.orange2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Orange2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
